0dca82224e08093991597c38e840dd219892c6c0
Mauricio Jost <mauricio.jost@gmail.com>
2020-02-10 13:33:38 +0100

    Light sleep

--- src/PlatformESP8266.h
+++ src/PlatformESP8266.h
@@ -206,6 +206,40 @@ void testArchitecture() {}
 // Execution
 ///////////////////
 
+#define WAKE_UP_PIN 0 
+
+void printMillis() {
+  Serial.print(F("millis() = "));  // show that millis() isn't correct across most Sleep modes
+  Serial.println(millis());
+  Serial.flush();  // needs a Serial.flush() else it may not print the whole message before sleeping
+}
+
+
+void wakeupCallback() {  // unlike ISRs, you can do a print() from a callback function
+  printMillis();  // show time difference across sleep; millis is wrong as the CPU eventually stops
+  Serial.println(F("Woke from Light Sleep - this is the callback"));
+}
+
+void test5() {
+  // From https://github.com/esp8266/Arduino/pull/6989/files 
+  Serial.println(F("\n5th test - Timed Light Sleep, wake in 10 seconds"));
+  WiFi.mode(WIFI_OFF);  // you must turn the modem off; using disconnect won't work
+  printMillis();  // show millis() across sleep, including Serial.flush()
+  extern os_timer_t *timer_list;
+  timer_list = nullptr;  // stop (but don't disable) the 4 OS timers
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+  gpio_pin_wakeup_enable(GPIO_ID_PIN(WAKE_UP_PIN), GPIO_PIN_INTR_LOLEVEL);  // GPIO wakeup (optional)
+  // only LOLEVEL or HILEVEL interrupts work, no edge, that's an SDK or CPU limitation
+  wifi_fpm_set_wakeup_cb(wakeupCallback); // set wakeup callback
+  // the callback is optional, but without it the modem will wake in 10 seconds then delay(10 seconds)
+  // with the callback the sleep time is only 10 seconds total, no extra delay() afterward
+  wifi_fpm_open();
+  wifi_fpm_do_sleep(10E6);  // Sleep range = 10000 ~ 268,435,454 uS (0xFFFFFFE, 2^28-1)
+  delay(10e3 + 1); // delay needs to be 1 mS longer than sleep or it only goes into Modem Sleep
+  Serial.println(F("Woke up!"));  // the interrupt callback hits before this is executed
+}
+
+
 BotMode setupArchitecture() {
 
   // Let HW startup
@@ -216,6 +250,13 @@ BotMode setupArchitecture() {
   Serial.setTimeout(1000); // Timeout for read
   setupLog(logLine);
   log(CLASS_PLATFORM, Info, "Log initialized");
+
+
+  test5();
+  test5();
+  test5();
+
+  delay(10000);
 
   log(CLASS_PLATFORM, Debug, "Setup cmds");
   cmdBuffer = new Buffer(COMMAND_MAX_LENGTH);
