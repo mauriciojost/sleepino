111af0612d454ef4fb44b288d630353003c3a785
Mauricio Jost <mauriciojost@gmail.com>
2020-04-24 19:39:36 +0200

    Provide restore

--- src/PlatformESP.h
+++ src/PlatformESP.h
@@ -3,6 +3,16 @@
 
 #define QUESTION_ANSWER_TIMEOUT_MS 60000
 
+#define RESTORE_WIFI_SSID "assid"
+#define RESTORE_WIFI_PASS "apassword"
+#define RESTORE_URL "http://main4ino.martinenhome.com/main4ino/prd/firmwares/botino/" PLATFORM_ID "/content?version=LATEST"
+#define RESTORE_RETRIES 10
+
+void restore() { // to be invoked as last resource when things go wrong
+  bool s = initializeWifi(RESTORE_WIFI_SSID, RESTORE_WIFI_PASS, RESTORE_WIFI_SSID, RESTORE_WIFI_PASS, true, RESTORE_RETRIES);
+  updateFirmware(RESTORE_URL, STRINGIFY(PROJ_VERSION));
+}
+
 void askStringQuestion(const char *question, Buffer *answer) {
   log(CLASS_PLATFORM, User, "Question: %s", question);
   Serial.setTimeout(QUESTION_ANSWER_TIMEOUT_MS);
