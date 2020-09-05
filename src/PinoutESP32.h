4c5cd44d2ded059b7f6fb4d6625768fa02d4c501
Mauricio Jost <mauriciojost@gmail.com>
2020-09-05 10:33:47 +0200

    Define missing pinouts

--- src/PinoutESP32.h
+++ src/PinoutESP32.h
@@ -2,42 +2,11 @@
 
 // FROM: https://www.instructables.com/id/ESP32-Internal-Details-and-Pinout/
 
-#define GPIO0_PIN 0 // ACD11 TOUCH1
-#define GPIO1_PIN 1 // TX0
-#define GPIO2_PIN 2 // ADC12 TOUCH2
-#define GPIO3_PIN 3 // RX0
-#define GPIO4_PIN 4 // ADC10 TOUCH0
-#define GPIO5_PIN 5
-#define GPIO6_PIN 6
-#define GPIO7_PIN 7
-#define GPIO8_PIN 8
-#define GPIO9_PIN 9 // RX1
-#define GPIO10_PIN 10 // TX1
-#define GPIO11_PIN 11
-#define GPIO12_PIN 12 // ADC15 TOUCH5 SPI-MISO
-#define GPIO13_PIN 13 // ADC14 TOUCH4 SPI-MOSI
-#define GPIO14_PIN 14
-#define GPIO15_PIN 15 // ADC13 TOUCH3 SPI-SS
-#define GPIO16_PIN 16 // RX2
-#define GPIO17_PIN 17 // TX2
-#define GPIO18_PIN 18 // VSPI-SCK
-#define GPIO19_PIN 19 // VSPI-MISO
+#include <pinouts/PinoutESP32DevKit.h>
 
-#define GPIO21_PIN 21 // I2C-SDA
-#define GPIO22_PIN 22 // I2C-SCL
-#define GPIO23_PIN 23 // VSPI-MOSI
-
-#define GPIO25_PIN 25 // DAC1 ADC18
-#define GPIO26_PIN 26 // DAC2 ADC19
-#define GPIO27_PIN 27 // ADC17 TOUCH7
-
-#define GPIO32_PIN 32 // ADC4 TOUCH9
-#define GPIO33_PIN 33 // ADC5 TOUCH8
-#define GPIO34_PIN 34 // ADC6
-#define GPIO35_PIN 35 // ADC7
-#define GPIO36_PIN 36 // ADC0
-
-#define GPIO39_PIN 39 // ADC3
+// IO
+#define SERVO0_PIN GPIO22_PIN
+#define POWER_PIN  GPIO23_PIN
 
 // LCD
 #define LCD_CLK_PIN GPIO14_PIN
--- src/PinoutESP8266.h
+++ src/PinoutESP8266.h
@@ -1,42 +1,9 @@
 #ifdef ESP8266 // NODEMCU based on ESP8266
 
-#define GPIO0_PIN 0
-#define GPIO1_PIN 1
-#define GPIO2_PIN 2
-#define GPIO3_PIN 3
-#define GPIO4_PIN 4
-#define GPIO5_PIN 5
-#define GPIO6_PIN 6
-#define GPIO7_PIN 7
-#define GPIO8_PIN 8
-#define GPIO9_PIN 9
-#define GPIO10_PIN 10
-#define GPIO11_PIN 11
-#define GPIO12_PIN 12
-#define GPIO13_PIN 13
-#define GPIO14_PIN 14
-#define GPIO15_PIN 15
-#define GPIO16_PIN 16
-#define A0_PIN 17
+#include <pinouts/PinoutESP8266.h>
 
-// NODEMCU based on ESP8266 (human names)
-
-#define PIN_D0 GPIO16_PIN // working as OUTPUT, breaks deep sleep mode, NODE-MCU built-in !LED
-#define PIN_D1 GPIO5_PIN
-#define PIN_D2 GPIO4_PIN
-#define PIN_D3 GPIO0_PIN  // working as OUTPUT
-#define PIN_D4 GPIO2_PIN  // working as OUTPUT, ESP8266-12 built-in !LED
-#define PIN_D5 GPIO14_PIN // working as OUTPUT
-#define PIN_D6 GPIO12_PIN
-#define PIN_D7 GPIO13_PIN  // RXD2
-#define PIN_D8 GPIO15_PIN  // TXD2, working as OUTPUT
-#define PIN_D9 GPIO3_PIN   // RXDO,  if used will break serial communication (uC <- PC), working as OUTPUT
-#define PIN_D10 GPIO1_PIN  // TXDO, if used will break serial communication (uC -> PC), can work as OUTPUT
-#define PIN_D11 GPIO9_PIN  // SDD2 / SD2 // cannot be used at all, internal
-#define PIN_D12 GPIO10_PIN // SDD3 / SD3 // can be used as input only
-
+// IO
 #define SERVO0_PIN PIN_D7
-
 #define POWER_PIN PIN_D3
 
 // LCD
@@ -50,6 +17,5 @@
 
 // GPIO16_PIN -> REST if deep sleep enabled on ESP8266
 // GND -> capacitor 47uF -> Vcc
-
 
 #endif // ESP8266
